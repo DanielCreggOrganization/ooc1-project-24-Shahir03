8ea46be2d5d96a530ff31181f80fc246a144ce1e
Shahir03 <148056693+Shahir03@users.noreply.github.com>
2024-11-28 10:48:51 +0000

    Working on constructor

--- src/ie/atu/countrymanager/Country.java
+++ src/ie/atu/countrymanager/Country.java
@@ -2,6 +2,19 @@ package ie.atu.countrymanager;
 
 public class Country {
     //Instance Variables
-    private int idnumber; //Unique id
+    private int idNumber; //Unique id
+    private String name;
+    private float numberofCitizens;
+    private double averagesalaryEuro;//add average
+
+    //Constructor
+    public Country(int idNumber, String name, float numberofCitizens, double averagesalaryEuro){
+        this.idNumber = idNumber;
+        this.name = name;
+        this.numberofCitizens = numberofCitizens;
+        this.averagesalaryEuro = averagesalaryEuro;
+
+    }
+
 
 }
