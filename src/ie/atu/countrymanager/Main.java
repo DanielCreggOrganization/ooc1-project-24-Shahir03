15e46d52fbc412ff8791657bb86d89dd23969c5c
Shahir03 <148056693+Shahir03@users.noreply.github.com>
2024-11-28 13:32:29 +0000

    Created a scanner

--- src/ie/atu/countrymanager/Main.java
+++ src/ie/atu/countrymanager/Main.java
@@ -7,8 +7,17 @@ public class Main {
     public static void main(String[] args){
         
     //Create Object
-    Country CountryObject1= new country 1, "Rainbow", 85, 3.33);
+    Country countryObject1= new Country( 1, "Qatar", 85, 3.33);
+    System.out.println(countryObject1.getidNumber());
+    System.out.println(countryObject1.getName());
+    System.out.println(countryObject1.getnumberOfCitizens());
+    System.out.println(countryObject1.getaveragesalaryEuro());
 
+    //Create a Scanner
+    Scanner userInput = new Scanner(System.in);
+    int userSelection = 0; //Used to store user Menu option selection
+
+    
     }
 
 }
