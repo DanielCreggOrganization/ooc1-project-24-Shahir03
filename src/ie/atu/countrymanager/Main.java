c600f4e31f03b5a176bb8d25f7a1005314f4d84f
Shahir03 <148056693+Shahir03@users.noreply.github.com>
2024-11-28 13:39:12 +0000

    worked on the loop

--- src/ie/atu/countrymanager/Main.java
+++ src/ie/atu/countrymanager/Main.java
@@ -21,7 +21,17 @@ public class Main {
     //Create infinite while loop to display Menu after every command
     while(true){
         //Display Menu to console
-        
+        System.out.println("");
+        System.out.println("###############################");
+        System.out.println("# Studient Applicarion v0.9 #")
+        System.out.println("###############################");
+        System.out.println("(1) Add a Country.");
+        System.out.println("(2) Delete a Country.");
+        System.out.println("(3) Show total number of Countries.");
+        System.out.println("(4) Search for a Country by Country ID.");
+        System.out.println("(5) Quit.");
+        System.out.println("Select an option from 1 to 5 and press Enter");
+
     }
     }
 
