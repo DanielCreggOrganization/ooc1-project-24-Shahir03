f4cd1f039d736348c047578d0e5851b1f24908cf
Shahir03 <148056693+Shahir03@users.noreply.github.com>
2024-11-28 10:53:54 +0000

    doing the getter methods

--- src/ie/atu/countrymanager/Country.java
+++ src/ie/atu/countrymanager/Country.java
@@ -4,17 +4,26 @@ public class Country {
     //Instance Variables
     private int idNumber; //Unique id
     private String name;
-    private float numberofCitizens;
+    private float numberOfCitizens;
     private double averagesalaryEuro;//add average
 
     //Constructor
-    public Country(int idNumber, String name, float numberofCitizens, double averagesalaryEuro){
+    public Country(int idNumber, String name, float numberOfCitizens, double averagesalaryEuro){
         this.idNumber = idNumber;
         this.name = name;
-        this.numberofCitizens = numberofCitizens;
+        this.numberOfCitizens = numberOfCitizens;
         this.averagesalaryEuro = averagesalaryEuro;
-
     }
+
+    //Getter Methods
+    public int getidNumber(){
+        return this.idNumber;
+    }
+    public String getName(){
+        return this.name;
+    }
+
+    
 
 
 }
