f717b752bd95ec234962ae940585e10c1cb33ea9
Shahir03 <148056693+Shahir03@users.noreply.github.com>
2024-12-04 10:42:26 +0000

    Using boolean to delete a country by its ID

--- src/ie/atu/countrymanager/CountryLibrary.java
+++ src/ie/atu/countrymanager/CountryLibrary.java
@@ -15,5 +15,16 @@ public class CountryLibrary {
         countries.add(country);
     }
 
+    //Delete a Country by using its ID
+    public boolean deleteCountry(int idNumber){
+        for (int i = 0; i < countries.size(); i++) {
+            if (countries.get(i).getidNumber() == idNumber){
+                countries.remove(i);
+                return true;
+            }
+        }
+        return false; //Retrun false if country not Found
+    }
+
 
 }
