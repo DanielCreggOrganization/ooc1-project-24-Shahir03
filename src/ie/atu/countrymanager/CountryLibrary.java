e3bcf8695613fe658643787a7d59144fd23a3484
Shahir03 <148056693+Shahir03@users.noreply.github.com>
2024-12-04 10:38:07 +0000

    Add a Constructor

--- src/ie/atu/countrymanager/CountryLibrary.java
+++ src/ie/atu/countrymanager/CountryLibrary.java
@@ -1,5 +1,16 @@
 package ie.atu.countrymanager;
 
+import java.util.ArrayList;
+
 public class CountryLibrary {
+    private ArrayList<Country> countries;
+
+    //Adding a Constructor
+    public CountryLibrary(){
+        countries = new ArrayList<>();
+    }
+
+    //
+
 
 }
