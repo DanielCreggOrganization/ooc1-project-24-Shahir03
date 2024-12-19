7f2e4def67f5dea49a84eb805ee477e4e02a40dd
Shahir03 <148056693+Shahir03@users.noreply.github.com>
2024-12-19 15:27:53 +0000

    Create a new class and added the public classes

---
+++ src/ie/atu/countrymanager/GeographicalEntity.java
@@ -0,0 +1,13 @@
+package ie.atu.countrymanager;
+
+public class GeographicalEntity {
+  public abstract class GeographicalEntity {
+    private int idNumber;
+    private String name;
+
+    public GeographicalEntity(int idNumber, String name) {
+        this.idNumber = idNumber;
+        this.name = name;
+    }
+
+}
